958c770a9b5e0e3b47ab3f3b7218dc430eeb9dc1
agbaca <adamgb.us@gmail.com>
2016-10-27 22:31:22 -0500

    FinalProd

--- HW8Script.R
+++ HW8Script.R
@@ -6,24 +6,29 @@ return(Csquare)
 }
 #inputs 3,4
 pyth(3,4)
-asin<-asin(4/5)
-degreesA<-(asin*180)/pi
+asn<-asin(4/5)
+degreesA<-(asn*180)/pi
 degreesA
 degreesB<-90-degreesA
 degreesB
 TestTotal<-(degreesA+degreesB+90)
 TestTotal
-#inputs 3,4
+#inputs 1,1
 pyth(1,1)
-asin<-asin(1/1.414214)
+asn<-asin(1/1.414214)
+degreesA<-(asn*180)/pi
 degreesA
+degreesB<-90-degreesA
 degreesB
 TestTotal
-#inputs 3,4
+#inputs 12,4
 pyth(12,4)
-asin<-asin(12/12.64911)
+asn<-asin(12/12.64911)
+degreesA<-(asn*180)/pi
 degreesA
+degreesB<-90-degreesA
 degreesB
+TestTotal
 savehistory("~/GitHub/HW8/HWwk8code.Rhistory")
 ######Reset Pyth with variables, then ASIN(2 Large Values)
 #####Then degreessA degreesB
