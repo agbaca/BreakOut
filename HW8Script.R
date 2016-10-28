8ef561cc7eedee0fa2a5f36dc375db0528ba3358
agbaca <adamgb.us@gmail.com>
2016-10-27 19:29:46 -0500

    Script

---
+++ HW8Script.R
@@ -0,0 +1,18 @@
+pyth <- function(a,b) {
+Csquare <- sqrt((a*a)+(b*b))
+return(Csquare)
+}
+pyth(4,9)
+asin(9/10)
+degreesA<-(asin(9/10)*180)/pi
+degreesA
+pyth <- function(a,b) {
+Csquare <- sqrt((a*a)+(b*b))
+return(Csquare)
+}
+pyth(4,9)
+pythexb<-function(B){
+degreesB<-90-degreesA
+return(degreesB)
+}
+pythexb(9/10)
