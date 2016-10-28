efdd324ae32f77df4ec41c3bc95ab7fdb61a03c1
agbaca <adamgb.us@gmail.com>
2016-10-27 21:14:56 -0500

    newscript

--- HW8Script.R
+++ HW8Script.R
@@ -1,18 +1,29 @@
+#Function that runs the pythagorean theorem
+setwd("C:\\Users\\adamg_000\\Documents\\GitHub\\BreakOut")
 pyth <- function(a,b) {
 Csquare <- sqrt((a*a)+(b*b))
 return(Csquare)
 }
-pyth(4,9)
-asin(9/10)
-degreesA<-(asin(9/10)*180)/pi
+#inputs 3,4
+pyth(3,4)
+asin<-asin(4/5)
+degreesA<-(asin*180)/pi
 degreesA
-pyth <- function(a,b) {
-Csquare <- sqrt((a*a)+(b*b))
-return(Csquare)
-}
-pyth(4,9)
-pythexb<-function(B){
 degreesB<-90-degreesA
-return(degreesB)
-}
-pythexb(9/10)
+degreesB
+TestTotal<-(degreesA+degreesB+90)
+TestTotal
+#inputs 3,4
+pyth(1,1)
+asin<-asin(1/1.414214)
+degreesA
+degreesB
+TestTotal
+#inputs 3,4
+pyth(12,4)
+asin<-asin(12/12.64911)
+degreesA
+degreesB
+savehistory("~/GitHub/HW8/HWwk8code.Rhistory")
+######Reset Pyth with variables, then ASIN(2 Large Values)
+#####Then degreessA degreesB
